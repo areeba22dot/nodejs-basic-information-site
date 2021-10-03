a10513e80a0d08e149e8f9f38553892e862ed384
areeba22dot <91815562+areeba22dot@users.noreply.github.com>
2021-10-03 15:15:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+const express = require("express"); // Initialization of objects and variables
+const path = require("path"); 
 const app = express();
 
 app.use(express.static("public/"));
